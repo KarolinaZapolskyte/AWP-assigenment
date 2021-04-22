292e5941717516f05c190e4035c26a5c712abba3
Karolina <zapolskyte.karolina@gmail.com>
2021-04-22 19:24:10 +0200

    fix

--- server/src/routes.js
+++ server/src/routes.js
@@ -6,73 +6,47 @@ module.exports = (qaDB) => {
 
   /**** Routes ****/
   router.get("/questions",  async (req, res) => {
-    try {
-      const questions = await qaDB.getQuestions()
-      res.json(questions);
-    } catch (error) {
-      console.error("error:", error.message);
-    }
+    const questions = await qaDB.getQuestions()
+    res.json(questions);
   });
 
   router.get("/questions/:id", async (req, res) => {
-    try {
-      const question = await qaDB.getQuestion(req.params.id)
-      res.json(question);
-    } catch (error) {
-      console.error("error:", error.message);
-    }
+    const question = await qaDB.getQuestion(req.params.id)
+    res.json(question);
   });
 
   router.post("/questions", async (req, res) => {
-    try {
-      const newQuestion = await qaDB.createQuestion(req.body.title, req.body.desc, req.body.tags);
-      await newQuestion.save();
-      res.json(newQuestion);
-    } catch (error) {
-      console.error("error:", error.message);
-    }
+    const newQuestion = await qaDB.createQuestion(req.body.title, req.body.desc, req.body.tags);
+    await newQuestion.save();
+    res.json(newQuestion);
   });
 
 // ---------------------------------------------------------- //
   
   router.get("/answers", async (req, res) => {
-    try {
-      const answers = await qaDB.getAnswers()
-      res.json(answers);
-    } catch (error) {
-      console.error("error:", error.message);
-    }
+    const answers = await qaDB.getAnswers()
+    res.json(answers);
   });
 
   router.post("/answers", async (req, res) => {
-    try {
-      const newAnswer = await qaDB.createAnswer(parseInt(req.body.id), req.body.answer)
-      await newAnswer.save();
-      res.json(newAnswer);
-    } catch (error) {
-      console.error("error:", error.message);
-    }
+    const newAnswer = await qaDB.createAnswer(parseInt(req.body.id), req.body.answer)
+
+    await newAnswer.save();
+    res.json(newAnswer);
   });
   
 // ---------------------------------------------------------- //
 
   router.get("/scores", async (req, res) => {
-    try {
-      const scores = await qaDB.getScore()
-      res.json(scores);
-    } catch (error) {
-      console.error("error:", error.message);
-    }
+    const scores = await qaDB.getScore()
+    res.json(scores);
   });
 
   router.post("/scores", async (req, res) => {
-    try {
-      const newScore = await qaDB.createScore(parseInt(req.body.id), parseInt(req.body.score));
-      await newScore.save()
-      res.json(newScore);
-    } catch (error) {
-      console.error("error:", error.message);
-    }
+    const newScore = await qaDB.createScore(parseInt(req.body.id), parseInt(req.body.score));
+
+    await newScore.save()
+    res.json(newScore);
   });
   
 
