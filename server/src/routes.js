5c1fd76ebea39ce998d2e661e2f8669e94787372
Karolina <zapolskyte.karolina@gmail.com>
2021-04-22 19:19:52 +0200

    fix: error handling

--- server/src/routes.js
+++ server/src/routes.js
@@ -6,47 +6,73 @@ module.exports = (qaDB) => {
 
   /**** Routes ****/
   router.get("/questions",  async (req, res) => {
-    const questions = await qaDB.getQuestions()
-    res.json(questions);
+    try {
+      const questions = await qaDB.getQuestions()
+      res.json(questions);
+    } catch (error) {
+      console.error("error:", error.message);
+    }
   });
 
   router.get("/questions/:id", async (req, res) => {
-    const question = await qaDB.getQuestion(req.params.id)
-    res.json(question);
+    try {
+      const question = await qaDB.getQuestion(req.params.id)
+      res.json(question);
+    } catch (error) {
+      console.error("error:", error.message);
+    }
   });
 
   router.post("/questions", async (req, res) => {
-    const newQuestion = await qaDB.createQuestion(req.body.title, req.body.desc, req.body.tags);
-    await newQuestion.save();
-    res.json(newQuestion);
+    try {
+      const newQuestion = await qaDB.createQuestion(req.body.title, req.body.desc, req.body.tags);
+      await newQuestion.save();
+      res.json(newQuestion);
+    } catch (error) {
+      console.error("error:", error.message);
+    }
   });
 
 // ---------------------------------------------------------- //
   
   router.get("/answers", async (req, res) => {
-    const answers = await qaDB.getAnswers()
-    res.json(answers);
+    try {
+      const answers = await qaDB.getAnswers()
+      res.json(answers);
+    } catch (error) {
+      console.error("error:", error.message);
+    }
   });
 
   router.post("/answers", async (req, res) => {
-    const newAnswer = await qaDB.createAnswer(parseInt(req.body.id), req.body.answer)
-
-    await newAnswer.save();
-    res.json(newAnswer);
+    try {
+      const newAnswer = await qaDB.createAnswer(parseInt(req.body.id), req.body.answer)
+      await newAnswer.save();
+      res.json(newAnswer);
+    } catch (error) {
+      console.error("error:", error.message);
+    }
   });
   
 // ---------------------------------------------------------- //
 
   router.get("/scores", async (req, res) => {
-    const scores = await qaDB.getScore()
-    res.json(scores);
+    try {
+      const scores = await qaDB.getScore()
+      res.json(scores);
+    } catch (error) {
+      console.error("error:", error.message);
+    }
   });
 
   router.post("/scores", async (req, res) => {
-    const newScore = await qaDB.createScore(parseInt(req.body.id), parseInt(req.body.score));
-
-    await newScore.save()
-    res.json(newScore);
+    try {
+      const newScore = await qaDB.createScore(parseInt(req.body.id), parseInt(req.body.score));
+      await newScore.save()
+      res.json(newScore);
+    } catch (error) {
+      console.error("error:", error.message);
+    }
   });
   
 
